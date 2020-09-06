580025710dc90f8549f02d66fa0b13019b9a6a9b
Egor Moroz <44409352+yorrdt@users.noreply.github.com>
2020-09-06 20:30:48 +0300

    posts, new illustration

--- components/script/profile.js
+++ components/script/profile.js
@@ -1,5 +1,28 @@
 export default {
     layout: 'tight',
+    data() {
+        return {
+            posts: [],
+            text: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.text.trim()) {
+                let date = new Date();
+
+                const newText = {
+                    id: Date.now(),
+                    text: this.text,
+                    date: date
+                }
+
+                this.posts.push(newText);
+                this.posts.reverse();
+                this.text = ''
+            }
+        },
+    },
     head() {
         return {
             title: 'TeeMate · Profile'
